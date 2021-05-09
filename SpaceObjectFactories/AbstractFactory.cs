4e94b96ea638d73659343fa8066dfce64ed9d3a6
KrasimirMarinov40403z <83904197+KrasimirMarinov40403z@users.noreply.github.com>
2021-05-09 12:49:47 +0300

    Add files via upload

---
+++ SpaceObjectFactories/AbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceApp.SpaceObjectFactories
+{
+    abstract class AbstractFactory
+    {
+        public abstract SpaceObject.SpaceObject Create(String[] args);
+    }
+
+}
---
+++ SpaceObjectFactories/MoonFactory.cs
@@ -0,0 +1,15 @@
+﻿using SpaceApp.SpaceObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceApp.SpaceObjectFactories
+{
+    class MoonFactory : AbstractFactory
+    {
+        public override SpaceObject.SpaceObject Create(string[] args)
+        {
+            return new Moon(args[3], args[2]);
+        }
+    }
+}
